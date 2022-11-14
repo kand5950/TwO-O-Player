46a53f5d6ceaf6087f416c759a074ad15b6fcfaa
Anit <anittankandasamy@gmail.com>
2022-11-14 16:16:38 -0500

    Initialize Question class

--- question.rb
+++ question.rb
@@ -1,3 +1,14 @@
 class Question
-  puts "Question"
+  def initialize
+    @num1 = rand(0..20)
+    @num2 = rand(0..20)
+  end
+
+  def question
+    "What does #{@num1} plus #{@num2} equal?" 
+  end
+
+  def answer
+    @num1 + @num2
+  end
 end
