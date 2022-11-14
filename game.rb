232ad8edca4832d6aee54a045721987e621611b3
Anit <anittankandasamy@gmail.com>
2022-11-14 16:31:56 -0500

    initialize new player objects in game, new methods in Player

--- game.rb
+++ game.rb
@@ -1,9 +1,12 @@
 class Game
+  attr_accessor :player_one, :player_two
   def initialize
     @question = Question.new
+    @player_one = Player.new("player_one")
+    @player_two = Player.new("player_two")
   end
 
   def start_game
-    puts @question.question
+    puts player_one.greeting, player_two.greeting
   end
 end
--- player.rb
+++ player.rb
@@ -1,4 +1,21 @@
 class Player
-  def initialize
+  attr_accessor :name, :lives
+
+  def initialize(n)
+    @name = n
+    @lives = 3
   end
+
+  def greeting
+    "Welcome #{name}"
+  end
+
+  def loss
+    self.lives -= 1
+  end
+
+  def over
+    self.lives == 0
+  end
+
 end
